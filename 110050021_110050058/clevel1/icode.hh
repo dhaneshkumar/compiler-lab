9f41bcf2dcc8fea29113991e04fb9989d23cbd83
Dhanesh <dhaneshkr26@gmail.com>
2014-03-18 17:07:49 +0530

    compute

--- 110050021_110050058/clevel1/icode.hh
+++ 110050021_110050058/clevel1/icode.hh
@@ -50,6 +50,7 @@ typedef enum
 	a_op_r_r_o1,	/* r <- r op o1 */
 	a_op_r_o1_o2,	/* r <- o1 op o2 */ 
 	a_op_o1_o2_r,	/* r <- o1 op o2 */
+	a_br,
 	a_nsy		/* not specified yet */
 } Assembly_Format;
 
@@ -63,6 +64,7 @@ typedef enum
 	i_r_op_o1,	/* r <- o1 */
 	i_r_r_op_o1,	/* r <- r op o1 */
 	i_r_o1_op_o2,	/* r <- o1 op o2 */ 
+	i_br,
 	i_nsy		/* not specified yet */
 } Icode_Format;
 
@@ -79,7 +81,14 @@ typedef enum
 	load, 
 	imm_load, 
 	store, 
-	nop 
+	nop,
+	set_grt_than,
+	br_not_equal,
+	set_less_than,
+	set_less_equal,
+	set_grt_equal,
+	set_not_equal,
+	set_equal
 } Tgt_Op;
 
 ///////////////////////// Instruction Descriptor ///////////////////////////////////
@@ -224,6 +233,61 @@ public:
 
 	Ics_Opd * get_opd1();
 	void set_opd1(Ics_Opd * io);
+
+	Ics_Opd * get_result();
+	void set_result(Ics_Opd * io);
+
+	void print_icode(ostream & file_buffer);
+	void print_assembly(ostream & file_buffer);
+};
+
+class Compute_IC_Stmt: public Icode_Stmt
+{ 
+	Ics_Opd * opd1; 
+	Ics_Opd * opd2;   
+	Ics_Opd * result; 
+
+public:
+	Compute_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1,  Ics_Opd * opd2, Ics_Opd * result); 
+	~Compute_IC_Stmt() {} 
+	Compute_IC_Stmt & operator=(const Compute_IC_Stmt & rhs);
+
+	Instruction_Descriptor & get_inst_op_of_ics();
+
+	Ics_Opd * get_opd1();
+	void set_opd1(Ics_Opd * io);
+
+	Ics_Opd * get_opd2();
+	void set_opd2(Ics_Opd * io);
+
+	Ics_Opd * get_result();
+	void set_result(Ics_Opd * io);
+
+	void print_icode(ostream & file_buffer);
+	void print_assembly(ostream & file_buffer);
+};
+
+
+
+class Control_Flow_IC_Stmt: public Icode_Stmt
+{ 
+	Ics_Opd * opd1; 
+	Ics_Opd * opd2;   
+	Ics_Opd * result; 
+
+public:
+	Control_Flow_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1,  Ics_Opd * opd2); 
+	Control_Flow_IC_Stmt(Tgt_Op inst_op, Ics_Opd * opd1); 
+	~Control_Flow_IC_Stmt() {} 
+	Control_Flow_IC_Stmt & operator=(const Control_Flow_IC_Stmt & rhs);
+
+	Instruction_Descriptor & get_inst_op_of_ics();
+
+	Ics_Opd * get_opd1();
+	void set_opd1(Ics_Opd * io);
+
+	Ics_Opd * get_opd2();
+	void set_opd2(Ics_Opd * io);
 
 	Ics_Opd * get_result();
 	void set_result(Ics_Opd * io);
